8095de0e9251ae50a40bf16e873838249f04e546
kim <kimemmanuel004@gmail.com>
2022-08-23 15:54:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution in this file!
+const employee = {
+    name: "Winny",
+    streetAddress: "Gudele Block6",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key] : value};
+
+}  
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    
+} 
